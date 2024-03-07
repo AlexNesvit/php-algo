0faa49118364eb18264ebe4e32bc223580e4a38c
Afpa <alex.nesvit2000@gmail.com>
2024-03-07 14:25:14 +0100

    index.php mod

---
+++ public/common/images/pages/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Présentation PHP-algo</h1>
+    <p>Création d'un petit génerateur de mot de passe crypté</p>
+    <a href="keygenGenerator">KeyGenGenerator</a>
+</body>
+</html>
