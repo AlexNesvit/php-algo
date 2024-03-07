85d65911d6c6b416e2998a6282feeabf19df3510
Afpa <alex.nesvit2000@gmail.com>
2024-03-07 14:44:41 +0100

    keygenGenerator.php mod

---
+++ public/common/images/pages/keygenGenerator.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form ction="keygenGenerator" method="post">
+        <label for="password">Entrez le mot de pass:</label>
+        <input type="text" id="password" name="password" required>
+        <imput type="submit" value="Crypter">
+    </form>
+</body>
+</html>
